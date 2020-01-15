0a5197558dd12b37bc644fb666e6d3db0a458e92
fed-gren <deveb22@gmail.com>
2020-01-15 15:40:15 +0900

    solve: 문제 풀이 추가
    - 문자열 조작

---
+++ Codewars/7kyu/Mumbling.js
@@ -0,0 +1,27 @@
+/**
+ * URL: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+ * Type: String
+ */
+
+const { Test } = require('../../kata');
+
+const accum = text => text.split('').map((char, idx) => {
+  let newChar = '';
+  const upper = char.toUpperCase();
+  const lower = char.toLowerCase();
+  newChar += upper;
+  while(idx--) {
+    newChar += lower
+  }
+  return newChar;
+}).join('-');
+
+Test.describe("accum", function () {
+  Test.it("Basic tests", function () {
+    Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+  })
+})
